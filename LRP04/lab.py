e1131752d0c71cfb28c178f7d051f299f1553ef2
rdcorrales <rdcorrales@up.edu.ph>
2024-05-24 16:12:13 +0800

    EY

--- LRP04/lab.py
+++ LRP04/lab.py
@@ -5,10 +5,27 @@ import time
 import timeit
 import numpy as np
 import os
+import psutil
+import random
+
+def generate_y_test():
+    return ([53.1, 49.7, 48.4, 54.2, 54.9, 43.7, 47.2, 45.2, 54.4, 50.4])
+
+def generate_x_test():
+    array = []
+    x_array = [3.63, 3.02, 3.82, 3.42, 3.59, 2.87, 3.03, 3.46, 3.36, 3.3]
+    
+    for x in x_array:
+        temp_array = [x] * len(x_array)
+        array.append(temp_array)
+
+    return array
+
+
 
 def main():
     n, p, s, t = input("Enter n, p, s, t: ").split()
-    
+
     try:
         n = int(n)
         p = int(p)
@@ -16,109 +33,128 @@ def main():
         t = int(t)
     except ValueError:
         print("User input is not an integer")
+        return
 
-    host= '10.0.4.51'
-
-    if (s == 0):
-        master(host, p, n, t)
+    host = '127.0.0.1'
+    if s == 0:
+        run_master(host, p, n, t)
     else:
-        slave(host, p)
+        run_slave(host, p)
 
-def master(host, p, n, t):
-    M = np.random.randint(100, size=(n,n)).tolist()
-    submatrix = reshape_matrix(M, t)
+
+def run_master(host, port, matrix_size, num_slaves):
+    process = psutil.Process(os.getpid())
+    master_core = 0
+    process.cpu_affinity([master_core])
+    print(f"Master process is pinned to core {master_core}")
+
+    # matrix = np.random.randint(100, size=(matrix_size, matrix_size)).tolist()
+    #test
+    matrix = generate_x_test()
+    submatrices = split_matrix(matrix, num_slaves)
+    # print
+    # print(matrix)
+    # for submatrix in submatrices:
+    #     print(submatrix)
+    # print_matrix_elements(submatrices)
     
-    # print(M)
-    # print(submatrix)
+    # y_matrix = np.random.randint(100, size=matrix_size).tolist()
+    # test
+    y_matrix = generate_y_test()
+    #print
+    # print(y_matrix)
+
+    for submatrix in submatrices:
+        submatrix.append(y_matrix)
+
+    for submatrix in submatrices:
+        print(submatrix)
     
-    start = timeit.default_timer()
     
-    srvsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print('Server socket opened')
+    start_time = timeit.default_timer()
 
-    srvsocket.bind((host, p))
-    print('Bind to the local port')
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind((host, port))
+        server_socket.listen(num_slaves)
+        print('Server is listening for connections')
 
-    srvsocket.listen(t)
-    print('Started Listening')
+        time_dict = {}
+        threads = []
 
-    time_dict = {}
-    client_counter = 0
-    while client_counter != t:
-        print('Waiting for connections')
-        index = client_counter
-        cli, ip = srvsocket.accept()
-        variable = threading.Thread(target=NewClientSocketHandler, args=(cli, ip, pickle.dumps(submatrix[index]), start,  t, time_dict))
-        variable.start()    
-        client_counter+=1
-        variable.join()
-    
-    print("Time taken:", sum(time_dict.values()))
+        for i in range(num_slaves):
+            client_socket, client_address = server_socket.accept()
+            # print_matrix_elements(submatrices[i])
+            thread = threading.Thread(target=handle_client, args=(client_socket, client_address, pickle.dumps(submatrices[i])))
+            threads.append(thread)
+            thread.start()
 
-def NewClientSocketHandler(cli, ip, serialized_data, start, t, myDict):
-    
-    print('The new client has socket id: ', cli)
-    # cli.sendall(serialized_data)
-    cli.sendall(len(serialized_data).to_bytes(4, byteorder='big') + serialized_data)
-    
-    print('Message got from client', ip)
-    print(cli.recv(4096).decode())
+        for thread in threads:
+            thread.join()
 
-    stop = timeit.default_timer()
-    myDict[ip] = stop-start
-    # print("Time taken:", stop - start)
+    stop_time = timeit.default_timer()
 
-def slave(host, p):
-    clisocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # print("Total time taken:", sum(time_dict.values()))
+    print("Total time taken:", stop_time - start_time)
+
+def handle_client(client_socket, client_address, serialized_data):
+    with client_socket:
+        print(f'Connected to client {client_address}')
+        client_socket.sendall(len(serialized_data).to_bytes(4, byteorder='big') + serialized_data)
+        print(f'Sent data to client {client_address}')
+
+        ack_message = client_socket.recv(4096).decode()
+        print(f'Received acknowledgment from client {client_address}: {ack_message}')
+
+        # stop_time = timeit.default_timer()
+        # time_dict[client_address] = stop_time - start_time
+
+def run_slave(host, port):
+    process = psutil.Process(os.getpid())
+    available_cores = psutil.cpu_count(logical=False)
+    slave_core = random.randint(1, available_cores - 1)
+    process.cpu_affinity([slave_core])
+    print(f"Slave process is pinned to core {slave_core}")
 
     connected = False
     while not connected:
         try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+                client_socket.connect((host, port))
+                connected = True
+                print('Client has connected to the server')
 
-            clisocket.connect((host, p))
-            print('Client has been connected')
-            start = timeit.default_timer()
-            connected = True
-            
+                start_time = timeit.default_timer()
+
+                data_length = int.from_bytes(client_socket.recv(4), byteorder='big')
+                serialized_data = bytearray()
+
+                while len(serialized_data) < data_length:
+                    data_chunk = client_socket.recv(4096)
+                    serialized_data.extend(data_chunk)
+
+                data = pickle.loads(serialized_data)
+                y_matrix = data.pop(-1)
+                # print(f'Received data from server: {data}')
+                # print_matrix_elements(data)
+                print(data)
+                print(y_matrix)
+
+                client_socket.sendall(b'ack')
+
+                stop_time = timeit.default_timer()
+                print("Time taken:", stop_time - start_time)
+
         except ConnectionRefusedError:
             print("[INFO] Connection refused by the server. Retrying in 1 second...")
             time.sleep(1)
-            continue
-
-    print('The message from server')
-    # Receive the actual data
-    data_length = int.from_bytes(clisocket.recv(4), byteorder='big')
-    serialized_data = bytearray()
-    # while True:
-    #     data_chunk = clisocket.recv(4096)
-    #     serialized_data.extend(data_chunk)
-    #     if (len(data_chunk) <4096):
-    #         break
-    while len(serialized_data) < data_length:
-        data_chunk = clisocket.recv(4096)
-        serialized_data.extend(data_chunk)
-
-    try:
-        data = pickle.loads(serialized_data)
-        # print(data)
-    except pickle.UnpicklingError as e:
-        print(f'Error occured while unpickling: {e}')
-    
-    msg = 'ack'
-    clisocket.send(msg.encode())
-
-    stop = timeit.default_timer()
-    print("Time taken:", stop - start)
-    clisocket.close()
 
 
-def reshape_matrix(matrix, num_slaves):
+def split_matrix(matrix, num_slaves):
     n = len(matrix)
-    cols_per_submatrix = n // num_slaves 
+    cols_per_submatrix = n // num_slaves
     remainder_cols = n % num_slaves
     submatrices = []
 
-    # Split the matrix into submatrices
     start_col = 0
     for i in range(num_slaves):
         extra_cols = 1 if i < remainder_cols else 0
@@ -130,6 +166,15 @@ def reshape_matrix(matrix, num_slaves):
         start_col = end_col
 
     return submatrices
+
+def print_matrix_elements(matrix):
+    if not matrix:
+        print("Matrix is empty")
+        return
+    first_element = matrix[0]
+    last_element = matrix[-1]
+    middle_element = matrix[len(matrix) // 2]
+    print(f"First element: {first_element}, Middle element: {middle_element}, Last element: {last_element}")
 
 if __name__ == "__main__":
     main()
